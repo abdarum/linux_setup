b757fd206fdfd1633a3fcb2f909d6f7030aab284
Kornel Stefańczyk <kornel@kornel.laptop>
2019-03-01 11:50:39 +0100

    working version

--- config.sh
+++ config.sh
@@ -5,27 +5,55 @@
 function disp_help {
 	echo "    Script: $0"
 	echo "Options:"
+	echo "-a - install most usage programs"
 	echo "-b - install boot-repair"
+	echo "-s - setup ssh servers"
 	echo "-h - display help"
+}
 
-rm -rf ~/.ssh/
-cp -rf ./PersonalData/Config/.ssh ~/
+function install_programs {
+	sudo apt-get update
+	sudo apt-get install vim
+	sudo apt-get install git
+	sudo apt-get install gimp
+	sudo apt-get install chromium
+}
 
-while getops bh name; do
-	case $name in
-		b)
-			echo "Install boot-repair program"
+function install_boot_repair {
+	echo "Install boot-repair program"
 
-			sudo add-apt-repository ppa:yannubuntu/boot-repair
-			sudo apt-get update
-			sudo apt-get install -y boot-repair && boot-repair
-			;;
-		h)
-			disp_help
-			;;
-		*)
-			echo ""
-	esac
-done
+	sudo add-apt-repository ppa:yannubuntu/boot-repair
+	sudo apt-get update
+	sudo apt-get install -y boot-repair && boot-repair
+}
 
+function setup_ssh {
+	mkdir -p ~/.ssh/
+	rm -rf ~/.ssh/*
+	cp -rf config_ssh_setup_file ~/.ssh/config
+}
+
+if [ -z "$1" ] ; then
+	disp_help
+else
+	while getopts "absh" name; do
+		case "$name" in
+			a)	
+				echo "Install most usage programs"
+				install_programs
+				;;
+			b)
+				echo "Install boot repair"
+				install_boot_repair	
+				;;
+			s)
+				echo "SSH server set up"
+				setup_ssh
+				;;		
+			h|*)
+				disp_help
+				;;
+		esac
+	done
+fi
 
